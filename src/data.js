1c857de3ea39d66c8e19e00902b238fa324d54bd
AryanDAG <128514663+AryanDAG@users.noreply.github.com>
2024-10-01 17:04:51 +0530

    Testimonial-Slider

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Pranay Gupta",
+        job: "Software Engineer",
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I have the pleasure of working with this team on several projects, and i am considered",
+    },
+    {
+        id: 2,
+        name: "Abir Pal",
+        job: "Graphic Designer",
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text: "I have the pleasure of working with this team on several projects, and i am considered",
+    },
+    {
+        id: 3,
+        name: "Soumya Banerjee",
+        job: "Marketing Manager",
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text: "I have the pleasure of working with this team on several projects, and i am considered",
+    },
+    {
+        id: 4,
+        name: "Saikat Mukherjee",
+        job: "Content Strategist",
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text: "I have the pleasure of working with this team on several projects, and i am considered",
+    },
+    {
+        id: 5,
+        name: "Aritra Biswas",
+        job: "Data Analyst",
+        image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+        text: "I have the pleasure of working with this team on several projects, and i am considered",
+    },
+    
+]
+export default reviews;
